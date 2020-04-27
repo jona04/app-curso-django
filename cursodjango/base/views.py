9396e614c9b9c1505dc17cf0ac2c08684a38eadc
jonatasilva <jonatas.iw@gmail.com>
2020-04-27 16:10:32 -0300

    adicionar debug toolbar

--- cursodjango/base/views.py
+++ cursodjango/base/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Ola django')
+    return HttpResponse('<html><body>Ola django</body></html>',content_type='text/html')
